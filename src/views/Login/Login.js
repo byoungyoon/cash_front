81370b191c3e327d42657f4251804f837cb7cb87
byoungyoon <70301218+byoungyoon@users.noreply.github.com>
2021-04-10 23:49:03 +0900

    cash project [21.4.10]
    1. 회원 가입 페이지 구성
    
    2. 로그인 페이지에 auth로 통한 페이지 판단

--- src/views/Login/Login.js
+++ src/views/Login/Login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import clsx from 'clsx';
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -17,6 +17,7 @@ import { InputLabel} from "@material-ui/core";
 import Button from '@material-ui/core/Button';
 import Icon from '@material-ui/core/Icon';
 import Checkbox from '@material-ui/core/Checkbox';
+import Select from '@material-ui/core/Select';
 
 const styles={
     bodyStyle: {
@@ -31,15 +32,21 @@ const styles={
         marginBottom: "1em",
         fontWeight: "500",
         fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
-        fontSize: "3em"
+        fontSize: "2em"
     },
     inputStyle: {
         width: "60%",
     },
     rememberStyle: {
         marginLeft: "14em"
-    }
-
+    },
+    formControl: {
+        //margin: theme.spacing(1),
+        minWidth: 120,
+      },
+    selectEmpty: {
+        //marginTop: theme.spacing(2),
+    },
 };
 
 const useStyles = makeStyles(styles);
@@ -50,20 +57,34 @@ export default function Login(){
     const [values, setValues] = React.useState({
         password: '',
         showPassword: false,
-      });
+        gender: '',
+    });
+
+    const [signup, setSingup] = useState(false);
     
-      const handleChange = (prop) => (event) => {
-        setValues({ ...values, [prop]: event.target.value });
-      };
+    const handleChange = (prop) => (event) => {
+      setValues({ ...values, [prop]: event.target.value });
+    };
+
+    const handleChangeByGender = (e) => {
+        const name = e.target.name;
+        setValues({
+            ...values,
+            [name]: e.target.value
+        });
+    }
     
-      const handleClickShowPassword = () => {
-        setValues({ ...values, showPassword: !values.showPassword });
-      };
+    const handleClickShowPassword = () => {
+      setValues({ ...values, showPassword: !values.showPassword });
+    };
     
-      const handleMouseDownPassword = (event) => {
-        event.preventDefault();
-      };
-    
+    const handleMouseDownPassword = (event) => {
+      event.preventDefault();
+    };
+
+    const handleClickToSignup = () => {
+        setSingup(signup => !signup);
+    };
 
     return(
         <div>
@@ -71,10 +92,11 @@ export default function Login(){
               <GridItem xs={3} sm={3} md={3} />
               <GridItem xs={6} sm={6} md={6}>
                   <Card>
+                      {!signup && (
                       <CardBody className={classes.bodyStyle}>
                       <h3 className={classes.titleStyle}>Log in</h3>
                         <div className={classes.formGroup}>
-                            <TextField className={classes.inputStyle} id="standard-basic" label="Email" variant="outlined" />
+                            <TextField className={classes.inputStyle} id="standard-basic" label="UserId" variant="outlined" />
                         </div>
 
                         <div className={classes.formGroup}>
@@ -117,10 +139,61 @@ export default function Login(){
                                 Sign in
                             </Button>
                             <p className="forgot-password text-right">
-                                Forgot <a href="#">password?</a>
+                                Forgot <a href="#">password</a> | <a href="#" onClick={handleClickToSignup}>sign up</a>
                             </p>
                         </div>
                       </CardBody>
+                      )}
+                      {signup && (
+                          <CardBody className={classes.bodyStyle}>
+                              <div>
+                                <h3 className={classes.titleStyle}>Sign up</h3>
+                                <div className={classes.formGroup}>
+                                    <TextField className={classes.inputStyle} label="UserId" variant="outlined" />
+                                </div>
+                                <div className={classes.formGroup}>
+                                    <TextField className={classes.inputStyle} label="Password" variant="outlined" />
+                                </div>
+                                <div className={classes.formGroup}>
+                                    <TextField className={classes.inputStyle} label="Name" variant="outlined" />
+                                </div>
+                                <div className={classes.formGroup}>
+                                    <FormControl variant="outlined" className={classes.formControl ,classes.inputStyle}>
+                                        <InputLabel htmlFor="age-native-simple">Gender</InputLabel>
+                                        <Select
+                                        native
+                                        value={values.gender}
+                                        onChange={handleChangeByGender}
+                                        inputProps={{
+                                            name: 'gender',
+                                            id: 'age-native-simple',
+                                        }}
+                                        >
+                                        <option aria-label="None" value="" />
+                                        <option value={"Man"}>Man</option>
+                                        <option value={"Woman"}>Woman</option>
+                                        </Select>   
+                                    </FormControl>
+                                </div>
+                                <div className={classes.formGroup}>
+                                    <TextField className={classes.inputStyle} label="Phone" variant="outlined" />
+                                </div>
+                                <div className={classes.formGroup}>
+                                    <Button 
+                                        variant="contained"
+                                        endIcon={<Icon>send</Icon>}
+                                        className={classes.inputStyle}
+                                        size="large"
+                                    >
+                                        Sign in
+                                    </Button>
+                                    <p className="forgot-password text-right">
+                                        <a href="#" onClick={handleClickToSignup}>Back</a>
+                                    </p>
+                                </div>
+                              </div>
+                          </CardBody>
+                      )}
                   </Card>
               </GridItem>
           </GridContainer>
