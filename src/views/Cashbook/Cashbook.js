a26f7613fe50b0e44032eccc378a3057dba5aba6
byoungyoon <70301218+byoungyoon@users.noreply.github.com>
2021-04-23 22:24:19 +0900

    cash project [21.4.23]
    1. Cashbook 페이지에 달력 구현
    
    2. 캘린더에 대한 css에 적용

--- src/views/Cashbook/Cashbook.js
+++ src/views/Cashbook/Cashbook.js
@@ -8,6 +8,10 @@ import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 import Moment from 'moment';
+import IconButton from '@material-ui/core/IconButton';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft'; 
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import { blackColor } from "assets/jss/material-dashboard-react";
 
 const styles = {
     cardCategoryWhite: {
@@ -37,9 +41,35 @@ const styles = {
         lineHeight: "1"
       }
     },
-    buttonStyle: {
+    tableStyle: {
+      "& table, & tbody": {
+        width: "100%",
+      },
+      "& td": {
+        height: "80px",
+        padding: "10px",
+        verticalAlign: "top",
+        "& span":{
+          fontSize: "1.2em",
+          fontWeight: "500",
+          fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+        }
+      },
+    },
+    center: {
       textAlign: "center",
-      marginTop: "1em"
+      fontSize: "1.7em",
+      fontWeight: "400",
+      fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+    },
+    colorRed: {
+      color: "red"
+    },
+    colorBlack: {
+      color: "black"
+    },
+    colorBlue: {
+      color: "blue"
     }
   };
 
@@ -50,6 +80,46 @@ export default function Cashbook() {
 
     const [getMoment, setMoment] = useState(Moment());
     const today = getMoment;
+    
+    const firstWeek = today.clone().startOf('month').week();
+    const lastWeek = today.clone().endOf('month').week() == 1 ? 53: today.clone().endOf('month').week();
+
+    const calendarArr = () => {
+      let result = [];
+      let week = firstWeek;
+      for(week; week<=lastWeek; week++){
+        result = result.concat(
+          <tr key={week}>
+            {
+              Array(7).fill(0).map((data, index)=>{
+                let days = today.clone().startOf('year').week(week).startOf('week').add(index, 'day');
+                
+                if(Moment().format('YYYYMMDD') === days.format('YYYYMMDD')){
+                  return(
+                    <td key={index} style={{backgroundColor: '#D1B2FF'}}>
+                      <span>{days.format('D')}</span>
+                    </td>
+                  );
+                } else if(days.format('MM') !== today.format('MM')){
+                  return(
+                    <td key={index} style={{backgroundColor:'#D5D5D5', color: '#A6A6A6'}}>
+                      <span>{days.format('D')}</span>
+                    </td>
+                  );
+                } else{
+                  return(
+                    <td key={index} style={{backgroundColor: '#F6F6F6'}}>
+                      <span className={index===0?classes.colorBlue: index===6?classes.colorRed: classes.colorBlack}>{days.format('D')}</span>
+                    </td>
+                  );
+                }
+              })
+            }
+          </tr>
+        );
+      }
+      return result;
+    }
 
     const handleSubstractClick = () => {
       setMoment(getMoment.clone().subtract(1, 'month'));
@@ -70,9 +140,22 @@ export default function Cashbook() {
                           </p>
                       </CardHeader>
                       <CardBody>
-                          <button onClick={handleSubstractClick}>이전 달</button>
-                          <span>{today.format('YYYY년 MM월')}</span>
-                          <button onClick={handleAddClick}>다음 달</button>
+                          <div className={classes.center}>
+                            <IconButton onClick={handleSubstractClick}>
+                              <ChevronLeftIcon />
+                            </IconButton>
+                            <span>{today.format('YYYY년 MM월')}</span>
+                            <IconButton onClick={handleAddClick}>
+                              <ChevronRightIcon />
+                            </IconButton>
+                          </div>
+                          <div className={classes.tableStyle}>
+                            <table>
+                              <tbody>
+                                {calendarArr()}
+                              </tbody>
+                            </table>
+                          </div>
                       </CardBody>
                   </Card>
               </GridItem>
